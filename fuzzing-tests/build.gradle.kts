f64da17e71d3ea4e519a9254f313d089f0b8eac6
yawkat <jonas.konrad@oracle.com>
2024-11-26 15:33:19 +0100

    remove agent override

--- fuzzing-tests/build.gradle.kts
+++ fuzzing-tests/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.time.Duration
+
 plugins {
     id("io.micronaut.build.internal.fuzzing-module")
     id("io.micronaut.internal.jazzer")
@@ -22,7 +24,6 @@ dependencies {
 }
 
 tasks.named<io.micronaut.internal.jazzer.PrepareClusterFuzzTask>("prepareClusterFuzz") {
-    agent.set(File("/home/yawkat/bin/jazzer/0.22.1/jazzer_standalone.jar"))
     targetClasses.set(listOf(
         //"io.micronaut.fuzzing.http.HttpTarget",
         "io.micronaut.fuzzing.http.EmbeddedHttpTarget",
@@ -43,12 +44,44 @@ tasks.named<io.micronaut.internal.jazzer.JazzerTask>("jazzer") {
     jvmArgs.set(listOf(
         "-Xmx512M",
         "-Dio.netty.leakDetection.level=paranoid",
-        "-Dio.netty.leakDetection.targetRecords=100"
+        "-Dio.netty.leakDetection.targetRecords=100",
+        "-XX:+ExitOnOutOfMemoryError",
+        "-XX:+HeapDumpOnOutOfMemoryError",
     ))
-    //instrumentationIncludes.set(listOf("io.micronaut.**"))
-    //instrumentationIncludes.set(listOf("io.netty.**"))
+    rssLimitMb.set(8192)
+    instrumentationIncludes.set(listOf("io.micronaut.**", "io.netty.**"))
     //forks.set(8)
     // todo: store in repo
     corpus.set(File("/home/yawkat/dev/scratch/go-fuzz-corpus/httpreq/corpus"))
-    //minimizeCrashFile.set(File("crash-cca31614e76829eed48909625efe6b36c150efbe"))
+    //minimizeCrashFile.set(File("crash-f8c58ba04554c9659bc85f1e750334a5138396e2"))
+    maxTotalTime.set(Duration.ofHours(2))
+    //maxTotalTime.set(Duration.ofSeconds(10))
+    coverageDumpFile.set(layout.buildDirectory.file("cov-report.exec"))
+}
+
+val jazzerReportDir = layout.buildDirectory.dir("jacocoJazzerHtml")
+
+tasks.create("jacocoJazzerReport", JacocoReport::class.java) {
+    executionData(layout.buildDirectory.file("cov-report.exec"))
+    classDirectories.from(files(sourceSets.main.get().runtimeClasspath.files.map { dir ->
+        if (dir.isFile) {
+            zipTree(dir).matching { exclude("META-INF/**") }
+        } else {
+            fileTree(dir) { exclude("META-INF/**") }
+        }
+    }))
+    reports {
+        xml.required = false
+        csv.required = false
+        html.required = true
+        html.outputLocation = jazzerReportDir
+    }
+    dependsOn("jazzer")
+}
+
+tasks.create("jacocoJazzerReportTar", Tar::class.java) {
+    archiveFileName = "coverage-report.tar.bz2"
+    compression = Compression.BZIP2
+    from(jazzerReportDir)
+    dependsOn("jacocoJazzerReport")
 }
