50355ae38ba0a755ba14b7a47bd0a9757fdf10d3
yawkat <jonas.konrad@oracle.com>
2024-11-26 17:09:39 +0100

    build script progress

--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -5,9 +5,10 @@ set -e
 mkdir -p ~/.gradle
 echo "auto.include.git.dirs=$(pwd)" >> ~/.gradle/gradle.properties
 
+cd micronaut-fuzzing
+
 # bug in micronaut-build
 mkdir -p checkouts
 touch checkouts/catalog-micronaut-core.sha1
 
-cd micronaut-fuzzing
 ./gradlew micronaut-fuzzing-tests:prepareClusterFuzz
