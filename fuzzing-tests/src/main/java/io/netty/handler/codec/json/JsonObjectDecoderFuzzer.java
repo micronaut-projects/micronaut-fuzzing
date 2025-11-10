6ebd73f6e9a42bdfc57ca1798ae58d9858a16094
yawkat <jonas.konrad@oracle.com>
2025-11-10 08:52:09 +0100

    use onException in JsonObjectDecoderFuzzer

--- fuzzing-tests/src/main/java/io/netty/handler/codec/json/JsonObjectDecoderFuzzer.java
+++ fuzzing-tests/src/main/java/io/netty/handler/codec/json/JsonObjectDecoderFuzzer.java
@@ -4,8 +4,6 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import io.micronaut.fuzzing.FuzzTarget;
 import io.micronaut.fuzzing.HttpDict;
 import io.micronaut.fuzzing.runner.LocalJazzerRunner;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.HandlerFuzzerBase;
 import io.netty.handler.codec.CorruptedFrameException;
 import io.netty.handler.codec.TooLongFrameException;
@@ -17,21 +15,20 @@ import javax.net.ssl.SSLException;
 public class JsonObjectDecoderFuzzer extends HandlerFuzzerBase {
     public JsonObjectDecoderFuzzer(FuzzedDataProvider fuzzedDataProvider) {
         channel.pipeline()
-            .addLast(new JsonObjectDecoder(fuzzedDataProvider.consumeInt(10, 1024), fuzzedDataProvider.consumeBoolean()))
-            .addLast(new ChannelInboundHandlerAdapter() {
-                @Override
-                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                    if (cause instanceof TooLongFrameException || cause instanceof CorruptedFrameException) {
-                        return;
-                    }
-                    super.exceptionCaught(ctx, cause);
-                }
-            });
+            .addLast(new JsonObjectDecoder(fuzzedDataProvider.consumeInt(10, 1024), fuzzedDataProvider.consumeBoolean()));
     }
 
     public static void fuzzerTestOneInput(FuzzedDataProvider fuzzedDataProvider) throws SSLException {
         var fuzzer = new JsonObjectDecoderFuzzer(fuzzedDataProvider);
         fuzzer.test(fuzzedDataProvider);
+    }
+
+    @Override
+    protected void onException(Exception e) {
+        if (e instanceof TooLongFrameException || e instanceof CorruptedFrameException) {
+            return;
+        }
+        super.onException(e);
     }
 
     public static void main(String[] args) {
