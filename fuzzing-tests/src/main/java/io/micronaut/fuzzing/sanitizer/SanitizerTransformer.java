1d4f5fa4e96bef7d78e94debc6116a440ff77cf7
yawkat <jonas.konrad@oracle.com>
2026-04-21 14:05:09 +0000

    Exclude Gradle worker classes from sanitizer retransformation

--- fuzzing-tests/src/main/java/io/micronaut/fuzzing/sanitizer/SanitizerTransformer.java
+++ fuzzing-tests/src/main/java/io/micronaut/fuzzing/sanitizer/SanitizerTransformer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.fuzzing.sanitizer;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
@@ -13,6 +28,7 @@ import java.lang.instrument.Instrumentation;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 import java.util.List;
+
 
 public class SanitizerTransformer implements AgentBuilder.Transformer {
     @Override
@@ -56,9 +72,11 @@ public class SanitizerTransformer implements AgentBuilder.Transformer {
             "com.code_intelligence",
             "com.sun",
             "net.bytebuddy",
+            "org.gradle",
             "sun",
             "java",
             "jdk",
+            "worker.org.gradle",
             "io.netty.buffer" // need to ignore nested calls to .array
         );
 
