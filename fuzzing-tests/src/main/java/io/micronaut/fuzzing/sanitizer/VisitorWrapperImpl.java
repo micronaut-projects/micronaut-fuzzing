c0d81ce4507cd94dad2c9d411f42a3534ef036d5
yawkat <jonas.konrad@oracle.com>
2026-04-24 11:20:15 +0000

    Add sanitizer CI debug logging

--- fuzzing-tests/src/main/java/io/micronaut/fuzzing/sanitizer/VisitorWrapperImpl.java
+++ fuzzing-tests/src/main/java/io/micronaut/fuzzing/sanitizer/VisitorWrapperImpl.java
@@ -37,6 +37,8 @@ import java.util.Arrays;
  */
 final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
     private static final String BOOLEAN_ARRAY = "[Z";
+    private static final boolean DEBUG = Boolean.parseBoolean(System.getenv("FUZZING_SANITIZER_DEBUG"));
+    private static final String DEBUG_CLASS = System.getenv().getOrDefault("FUZZING_SANITIZER_DEBUG_CLASS", "io/netty/util/ByteProcessor");
 
     private static final Method BALOAD;
     private static final Method BASTORE;
@@ -60,20 +62,23 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
 
     @Override
     public ClassVisitor wrap(TypeDescription typeDescription, ClassVisitor classVisitor, Implementation.Context context, TypePool typePool, FieldList<FieldDescription.InDefinedShape> fieldList, MethodList<?> methodList, int i, int i1) {
-        return new ClassVisitorImpl(Opcodes.ASM9, classVisitor);
+        return new ClassVisitorImpl(Opcodes.ASM9, classVisitor, typeDescription.getInternalName());
     }
 
     private static final class ClassVisitorImpl extends ClassVisitor {
+        private final String owner;
         private boolean hasBooleanArrayField;
 
-        ClassVisitorImpl(int api, ClassVisitor classVisitor) {
+        ClassVisitorImpl(int api, ClassVisitor classVisitor, String owner) {
             super(api, classVisitor);
+            this.owner = owner;
         }
 
         @Override
         public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
             if (BOOLEAN_ARRAY.equals(descriptor)) {
                 hasBooleanArrayField = true;
+                debug(owner, "field boolean[] detected: " + name + ' ' + descriptor);
             }
             return super.visitField(access, name, descriptor, signature, value);
         }
@@ -82,29 +87,38 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
         public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
             MethodVisitorImpl methodVisitor = new MethodVisitorImpl(
                 Opcodes.ASM9,
-                super.visitMethod(access, name, descriptor, signature, exceptions)
+                super.visitMethod(access, name, descriptor, signature, exceptions),
+                owner,
+                name
             );
             if (descriptor.contains(BOOLEAN_ARRAY)) {
                 methodVisitor.ohNoBooleanArray = true;
+                debug(owner, name + " boolean[] from method descriptor: " + descriptor);
             }
             if ("<clinit>".equals(name) && hasBooleanArrayField) {
                 methodVisitor.ohNoBooleanArray = true;
+                debug(owner, name + " boolean[] from class field presence");
             }
             return methodVisitor;
         }
     }
 
     private static final class MethodVisitorImpl extends MethodVisitor {
+        private final String owner;
+        private final String methodName;
         private boolean ohNoBooleanArray;
 
-        MethodVisitorImpl(int api, MethodVisitor methodVisitor) {
+        MethodVisitorImpl(int api, MethodVisitor methodVisitor, String owner, String methodName) {
             super(api, methodVisitor);
+            this.owner = owner;
+            this.methodName = methodName;
         }
 
         @Override
         public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
             if (!ohNoBooleanArray && BOOLEAN_ARRAY.equals(descriptor)) {
                 ohNoBooleanArray = true;
+                debug(this.owner, methodName + " boolean[] from field access: " + owner + '.' + name + ' ' + descriptor);
             }
             super.visitFieldInsn(opcode, owner, name, descriptor);
         }
@@ -113,6 +127,7 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
         public void visitIntInsn(int opcode, int operand) {
             if (!ohNoBooleanArray && opcode == Opcodes.NEWARRAY && operand == Opcodes.T_BOOLEAN) {
                 ohNoBooleanArray = true;
+                debug(owner, methodName + " boolean[] from NEWARRAY T_BOOLEAN");
             }
             super.visitIntInsn(opcode, operand);
         }
@@ -122,12 +137,14 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
             for (Object o : local) {
                 if (BOOLEAN_ARRAY.equals(o)) {
                     ohNoBooleanArray = true;
+                    debug(owner, methodName + " boolean[] from frame local: " + Arrays.toString(local) + " stack=" + Arrays.toString(stack));
                     break;
                 }
             }
             for (Object o : stack) {
                 if (BOOLEAN_ARRAY.equals(o)) {
                     ohNoBooleanArray = true;
+                    debug(owner, methodName + " boolean[] from frame stack: locals=" + Arrays.toString(local) + " stack=" + Arrays.toString(stack));
                     break;
                 }
             }
@@ -143,8 +160,10 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
             }
 
             if (opcode == Opcodes.BALOAD) {
+                debug(owner, methodName + " rewriting BALOAD");
                 invokeStatic(BALOAD);
             } else if (opcode == Opcodes.BASTORE) {
+                debug(owner, methodName + " rewriting BASTORE");
                 invokeStatic(BASTORE);
             } else {
                 super.visitInsn(opcode);
@@ -153,26 +172,29 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
 
         @Override
         public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-            // Also treat any method that mentions boolean[] in its signature (parameters or return)
-            // as an escape hatch source. This covers synthetic accessors returning [Z from outer classes.
             if (!ohNoBooleanArray && descriptor.contains(BOOLEAN_ARRAY)) {
                 ohNoBooleanArray = true;
+                debug(this.owner, methodName + " boolean[] from invoked method descriptor: " + owner + '.' + name + descriptor);
             }
             String arraysOwner = Type.getInternalName(Arrays.class);
             String systemOwner = Type.getInternalName(System.class);
 
             if (owner.equals(arraysOwner) && name.equals("copyOf")
                 && descriptor.equals(Type.getMethodDescriptor(Type.getType(byte[].class), Type.getType(byte[].class), Type.INT_TYPE))) {
+                debug(this.owner, methodName + " rewriting Arrays.copyOf(byte[]) ");
                 invokeStatic(ARRAYS_COPY_OF);
                 return;
             } else if (owner.equals(arraysOwner) && name.equals("copyOfRange")
                 && descriptor.equals(Type.getMethodDescriptor(Type.getType(byte[].class), Type.getType(byte[].class), Type.INT_TYPE, Type.INT_TYPE))) {
+                debug(this.owner, methodName + " rewriting Arrays.copyOfRange(byte[]) ");
                 invokeStatic(ARRAYS_COPY_OF_RANGE);
                 return;
             } else if (owner.equals(systemOwner) && name.equals("arraycopy")) {
+                debug(this.owner, methodName + " rewriting System.arraycopy");
                 invokeStatic(SYSTEM_ARRAYCOPY);
                 return;
             } else if (name.equals("array") && owner.equals(Type.getInternalName(ByteBuf.class))) {
+                debug(this.owner, methodName + " rewriting ByteBuf.array()");
                 invokeStatic(BYTE_BUF_ARRAY);
                 return;
             }
@@ -187,6 +209,12 @@ final class VisitorWrapperImpl extends AsmVisitorWrapper.AbstractBase {
                 Type.getMethodDescriptor(method),
                 false
             );
+        }
+    }
+
+    private static void debug(String owner, String message) {
+        if (DEBUG && DEBUG_CLASS.equals(owner)) {
+            System.err.println("[sanitizer-debug] " + owner + " :: " + message);
         }
     }
 }
